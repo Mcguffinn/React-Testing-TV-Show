7a6b18ee50c1a37e8bdee15270759ccf7ac2b20e
Mcguffinn <deronviledwin@gmail.com>
2020-02-24 19:48:04 -0500

    mvp i think

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,13 +1,9 @@
-// import axios from "axios";
-// import App from './App';
+import axios from "axios";
 
-// export const fetchShow = () => {
-//     axios
-//       .get(
-//         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-//       )
-//       .then(res => {
-//         setShow(res.data);
-//         setSeasons(formatSeasons(res.data._embedded.episodes));
-//       });
-//   };
+export const fetchShow = () => {
+    return axios.get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+        .then(res => res)
+        
+  }
